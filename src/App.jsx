3648bce829702b303b7e378b6577b5eb8cdaf627
HOONKH <boospa0305@gmai.com>
2023-11-10 17:43:15 +0900

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,48 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import CoinCard from "./components/CoinCard";
+import Slider from "react-slick";
+
 const App = () => {
-  return <div className="bg-purple-400">Hey CRA</div>;
+  const [coinPrices, setCoinPrices] = useState();
+  const getCoinPrices = async () => {
+    const response = await axios.get(
+      "https://api.upbit.com/v1/ticker?markets=KRW-BTC,KRW-ETH,KRW-MATIC,BTC-ASTR,KRW-EOS"
+    );
+
+    console.log(response);
+    setCoinPrices(response.data);
+  };
+
+  useEffect(() => {
+    getCoinPrices();
+  }, []);
+  return (
+    <div className="bg-purple-400 min-h-screen flex flex-col justify-center items-center">
+      {coinPrices ? (
+        <ul className="w-96">
+          <Slider
+            autoplay={true}
+            autoplaySpeed={2000}
+            arrows={false}
+            dots={true}
+          >
+            {coinPrices.map((v, i) => {
+              return (
+                <CoinCard
+                  key={i}
+                  tokenPrice={v}
+                  btcPrice={coinPrices[0].trade_price}
+                />
+              );
+            })}
+          </Slider>
+        </ul>
+      ) : (
+        <div>Loading...</div>
+      )}
+    </div>
+  );
 };
 
 export default App;
---
+++ src/components/CoinCard.jsx
@@ -0,0 +1,20 @@
+const CoinCard = ({ tokenPrice, btcPrice }) => {
+  return (
+    <li className="flex items-center justify-center text-xl gap-4">
+      <img
+        className="w-9 inline-block"
+        src={`./images/${tokenPrice.market.substring(4)}.png`}
+      />
+      <span className="inline-block w-12 ml-2 font-black">
+        {tokenPrice.market.substring(4)}
+      </span>
+      <span className="ml-2 font-black">
+        ₩{" "}
+        {tokenPrice.market.substring(0, 3) === "BTC"
+          ? (tokenPrice.trade_price * btcPrice).toFixed(2)
+          : tokenPrice.trade_price.toLocaleString()}
+      </span>
+    </li>
+  );
+};
+export default CoinCard;
